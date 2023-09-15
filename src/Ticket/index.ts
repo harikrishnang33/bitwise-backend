4494d34b44b082ec29f4ff0b3c4c26bdef79b925
Alwin Joseph <alwin@keyvalue.systems>
2023-09-15 18:53:32 +0530

    Guard in ticket controller

--- src/Ticket/index.ts
+++ src/Ticket/index.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CommonModule } from 'src/Common';
 import { TicketService } from './Services/TicketService';
 import { TicketController } from './Controllers/TicketController';
+import AuthModule from '../Auth';
 
 @Module({
-  imports: [CommonModule],
+  imports: [CommonModule, AuthModule],
   providers: [TicketService],
   controllers: [TicketController],
   exports: [TicketService],
