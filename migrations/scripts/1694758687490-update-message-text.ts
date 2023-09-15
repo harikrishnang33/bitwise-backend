4926f5fb9adc5cd88b60b5059d1e96f54006d76e
Harikrishnan G <harikrishnan.g@keyvalue.systems>
2023-09-15 11:57:39 +0530

    Updated message text

---
+++ migrations/scripts/1694758687490-update-message-text.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class UpdateMessageText1694758687490 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `ALTER TABLE public.message ALTER COLUMN message TYPE text USING message::text`,
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        // no implementation
+    }
+
+}
